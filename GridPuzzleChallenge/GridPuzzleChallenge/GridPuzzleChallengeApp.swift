c1a088156a81b409e2131988689eb85aea89e1cd
Muhammad Athif <muhammadathief0@gmail.com>
2023-05-18 22:58:20 +0700

    Initial commit

---
+++ GridPuzzleChallenge/GridPuzzleChallenge/GridPuzzleChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridPuzzleChallengeApp.swift
+//  GridPuzzleChallenge
+//
+//  Created by Muhammad Athif on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GridPuzzleChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
